dbf97c64ce37e8af117ef84dbc8a3c257681f571
Noname <100673877+Noname968@users.noreply.github.com>
2024-11-23 23:47:41 -0700

    proxy

---
+++ src/routes/route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { hlsProxy, qualityProxy, segmentProxy } from '../controllers/proxy';
+import { hianimeHlsProxy, hianimeQaulityProxy, hianimeSegmentProxy } from '../controllers/hianime-proxy';
+
+export const router = express.Router();
+
+router.get('/proxy', hlsProxy);
+
+router.get('/quality-proxy', qualityProxy);
+
+router.get('/segment-proxy', segmentProxy);
+
+router.get('/hianime-hls-proxy', hianimeHlsProxy);
+
+router.get('/hianime-quality-proxy', hianimeQaulityProxy);
+
+router.get('/hianime-segment-proxy', hianimeSegmentProxy);
